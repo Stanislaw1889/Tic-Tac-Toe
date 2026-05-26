6439f719a1b184296261c955f4c2d5c2e282c7dc
Mateusz Wolan <mwolan06@gmail.com>
2026-05-27 01:04:00 +0200

    Uzupelnilem wspolny plik ustawienia.py o odpowiednie kolory i rozmiar okienka aplikacji

---
+++ ustawienia.py
@@ -0,0 +1,16 @@
+#WYMIARY OKNA GRY
+OKNO_SZEROKOSC = 800
+OKNO_WYSOKOSC = 600
+
+# GLOWNE KOLORY MENU
+
+CZERN_TLA = (15, 15, 18) # tlo menu
+ZIELEN = (20, 160, 90)    # kolor ramek do przyciskow i tekstow w tych przyciskach
+CZERWIEN = (220, 40, 60)  # kolor aktywnego elementu (gdy najedziemy na niego myszka)
+BIEL = (255, 255, 255)   # Np. do tytulow
+SZARY_TEKST = (100, 100, 100)  # Do mniej ważnych
+
+# --- PARAMETRY INTERFEJSU ---
+GRUBOŚĆ_RAMKI = 2       # Standardowa grubość obwódki przycisków
+ODSTĘP_PRZYCISKÓW = 20  # Odstep pionowy miedzy przyciskami ktory zachowuje w menu glownym
+                        # Jesli uznacie ze bedzie Wam pasowal inny odstep to nie trzymajcie sie tej wartosci
