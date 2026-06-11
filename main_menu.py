87d54125fdd189e30febe8d7b29a9a63c30e26cc
Mateusz Wolan <mwolan06@gmail.com>
2026-06-11 02:19:06 +0200

    Dodanie loga do menu glownego

--- main_menu.py
+++ main_menu.py
@@ -11,6 +11,15 @@ pygame.init()
 ekran = pygame.display.set_mode((ustawienia.OKNO_SZEROKOSC, ustawienia.OKNO_WYSOKOSC))
 pygame.display.set_caption("Tic-Tac-Toe")
 
+#Wczytuje logo gry
+try:
+    logo_raw = pygame.image.load("logo.png").convert()
+    logo_raw.set_colorkey((255, 255, 255))
+    WIELKOSC_LOGO = 180
+    logo_image = pygame.transform.smoothscale(logo_raw, (WIELKOSC_LOGO, WIELKOSC_LOGO))
+except pygame.error:
+    logo_image = None
+
 czcionka_przyciskow = pygame.font.SysFont("Arial", 26, bold=True)
 czcionka_nazwy_gry = pygame.font.SysFont("Arial", 38, bold=True)
 
@@ -57,9 +66,15 @@ def main_menu():
     
         ekran.fill(ustawienia.CZERN_TLA)
 
+        #Rysowanie loga
+        if logo_image:
+            X_logo = (ustawienia.OKNO_SZEROKOSC // 2) - (logo_image.get_width() // 2)
+            Y_logo = 15
+            ekran.blit(logo_image, (X_logo, Y_logo))
+
         tekst_nazwy = czcionka_nazwy_gry.render("TIC-TAC-TOE", True, ustawienia.BIEL)
         X_nazwy = (ustawienia.OKNO_SZEROKOSC // 2) - (tekst_nazwy.get_width() // 2)
-        Y_nazwy = 120
+        Y_nazwy = 210
         
         ekran.blit(tekst_nazwy, (X_nazwy, Y_nazwy))
 
